ff7d0323b2d226a417ccf37687b54b3a71365e02
sarthak75 <78541466+sarthak75@users.noreply.github.com>
2022-09-29 19:06:38 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is React.js?",
+    answer: "Free and open-source JavaScript front-end library",
+    options: [
+      "Open-source JavaScript back-end library",
+      "JavaScript front-end library to create a database",
+      "Free and open-source JavaScript front-end library",
+      "None of the mentioned"
+    ]
+  },
+    {
+    numb: 2,
+    question: "React.js is written in which of the following language??",
+    answer: "C",
+    options: [
+      "C",
+      "C++",
+      "Javascript",
+      "Java"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is used to pass data to a component from outside in React??",
+    answer: "props",
+    options: [
+      "props",
+      "render with arguments",
+      "setState",
+      "proptypes"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is correct about prop in react?",
+    answer: "Cannot be changed in the component",
+    options: [
+      "Can be changed inside another component",
+      "Can be changed inside the component",
+      "Cannot be changed in the component",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who created React.js?",
+    answer: "Jordan Walke",
+    options: [
+      "Jordan Mike",
+      "Jordan Walke",
+      "Tim Lee",
+      "Jordan Lee"
+    ]
+  },
+ 
+];
